b0927c10256abd79dbfd79eccbff9fe7d69ec7ec
asliddin-coder <88778317+asliddin-coder@users.noreply.github.com>
2021-08-11 19:34:10 +0500

    Create dars1.py
    birinchi dars javobi

---
+++ dars1.py
@@ -0,0 +1,12 @@
+#1. 5ning 4-darajasini toping
+print("5ning 4-darajasi ", 5**4)
+#2. 22 ni 4ga bo'lhanda necha qoldiq qoladi?
+print("22 ni 4ga bo'lhanda necha qoldiq " ,22%4)
+#3. Tomonlari 125ga teng kvadratning yuzini va perimetrini toping?
+print("yuzi:", 125**2) 
+print("perimetri: ", 125*4)
+#4. Deametri 12ga teng bo'lgan doirani yuzini toping? (pi ni 3.14 deb oling.)
+print("doira yuzi: ", 3.14*(12/2)**2)
+#5. Katetlari 6 va 7 ga teng bo'lgan to'griburchakli ucburchakning gepotinuzasini toping (pifagor teoremasidan foydalaning)
+import math
+print("gepotinuza: ", math.sqrt(6**2+7**2))
